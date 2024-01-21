5f10817929b0ac0b4d6635c651ad09615b693fb3
LankeyBoard <56012902+LankeyBoard@users.noreply.github.com>
2024-01-21 00:05:09 -0500

    Setting up rules layout style

--- fortunes-favor/app/components/Field.tsx
+++ fortunes-favor/app/components/Field.tsx
@@ -23,39 +23,53 @@ export function fieldCreator(json: any) {
     console.log("Error creating field from json", json);
     return null;
 }
+
+const titleStyler =(depth: number) => {
+    switch(depth) {
+        case 1:
+            return("text-3xl tracking-wide font-bold py-4 px-1");
+        case 2:
+            return("my-4 mx-2 text-2xl tracking-wide");
+        case 3:
+            return("text-lg p-2 font-semibold")
+    }
+}
+
 type fieldProps = {
     field: field_type;
     depth?: number;
 }
 const Field = ({field, depth=1}: fieldProps) => {
-    let rulesTitleClass = "Rules-header"+depth;
-    console.log("creating field for ", field);
     return(
-        <>
-        <div className={rulesTitleClass}>
-            {field.title}
-        </div>
-        {field.text !== undefined && 
-            <div>
-                {field.text}
+        <div id={field.slug}>
+            <div className={titleStyler(depth)}>
+                {field.title}
             </div>
-        }
-        {field.eg !== undefined && 
-            <div>
-                <p>e.g. <i>{field.eg}</i></p>
+            <div className="pb-2 mx-5">
+                {field.text !== undefined && 
+                    <div className="mb-2">
+                        {field.text}
+                    </div>
+                }
+                {field.eg !== undefined && 
+                    <div>
+                        <p>e.g. <i>{field.eg}</i></p>
+                    </div>
+                }
+                {field.list &&
+                    <ul>
+                        {field.list.map((f)=> <li key={f.slug}><Field field={f} depth={depth+1}></Field></li>)}
+                    </ul>
+                }
             </div>
-        }
-        {field.list &&
-            <ul>
-                {field.list.map((f)=> <li key={f.slug}><Field field={f} depth={depth+1}></Field></li>)}
-            </ul>
-        }
-        {field.rules !== undefined &&
-        <div>
-            {field.rules.map((f) => <Field field={f} depth={depth+1}></Field>)}
+            <div className={depth>1?"mx-4 bg-teal-900 border-white-200 border-l-2":""}>
+                {field.rules !== undefined &&
+                <div className="">
+                    {field.rules.map((f) => <Field field={f} depth={depth+1}></Field>)}
+                </div>
+                }
+            </div>
         </div>
-        }
-        </>
     )
 
 }
--- fortunes-favor/app/layout.tsx
+++ fortunes-favor/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="container mx-auto">{children}</body>
+      <body className="container mx-auto max-w-screen-xl">{children}</body>
     </html>
   )
 }
