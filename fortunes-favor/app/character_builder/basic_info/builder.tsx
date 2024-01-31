96e965977979daf3cc4ea7b7d0273659a9d4958a
LankeyBoard <56012902+LankeyBoard@users.noreply.github.com>
2024-01-31 09:18:03 -0500

    Added textarea to handle character questions

--- fortunes-favor/app/character_builder/basic_info/builder.tsx
+++ fortunes-favor/app/character_builder/basic_info/builder.tsx
@@ -1,10 +1,23 @@
+import InputArea from "@/app/components/blocks/InputArea";
 import InputField from "@/app/components/blocks/InputField";
 
 function BasicInfoBuilder(){
     return (
     <div className="p-10">
         Basic Info about the character
-        <InputField name="Character Level" type="number" defaultValue="1"/>
+        <div className="max-w-lg mx-auto">
+            <div className="flex flex-row gap-4 ">
+                <div className="basis-1/4">
+                    <InputField name="Level" type="number" defaultValue="1"/>
+                </div>
+                <div className="basis-3/4">
+                    <InputField name="Name" type="text"/>
+                </div>
+            </div>
+            <div className="mt-8">
+                <InputArea name="Character Question 1"/>
+            </div>
+        </div>
     </div>
     );
 }
