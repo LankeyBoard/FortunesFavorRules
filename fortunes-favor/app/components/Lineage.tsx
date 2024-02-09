b02837b1560a2522ee0006d12cc45415c6a0e4fc
LankeyBoard <56012902+LankeyBoard@users.noreply.github.com>
2024-02-08 21:13:43 -0500

    fixing linting

--- fortunes-favor/app/components/Lineage.tsx
+++ fortunes-favor/app/components/Lineage.tsx
@@ -1,34 +1,39 @@
 import CharacterLineage from "../utils/CharacterLineage";
-import Traits from "./Traits"
+import Traits from "./Traits";
 
 const Lineage = (json: any) => {
-    console.log("Lineage json input: ", json)
-    const l = new CharacterLineage(json.json);
-    return(
-        <div id={l.slug} className="mb-6">
-            <div className="my-4 text-2xl tracking-wide">
-                {l.title}
-            </div>
-            <div className="italic" >
-                {l.desc}
-            </div>
-            <div>
-                <span className="font-semibold">Size - </span> {typeof l.size === "string"
-                ? <span className="capitalize">{l.size.toLocaleLowerCase()}</span> 
-                : l.size.map((s) => {return(<span className="capitalize">{s.toLocaleLowerCase()} </span>)})}
-            </div>
-            <div>
-                <span className="font-semibold">Speed - </span>{l.speed}
-            </div>
-            <div>
-                <span className="font-semibold">Stat - </span>{l.stat}
-            </div>
-            <Traits title="Traits" traits={l.traits}/>
-            {l.options && 
-            <Traits title="Options" traits={l.options}/>
-            }
-        </div>
-    )
-}
+  console.log("Lineage json input: ", json);
+  const l = new CharacterLineage(json.json);
+  return (
+    <div id={l.slug} className="mb-6">
+      <div className="my-4 text-2xl tracking-wide">{l.title}</div>
+      <div className="italic">{l.desc}</div>
+      <div>
+        <span className="font-semibold">Size - </span>{" "}
+        {typeof l.size === "string" ? (
+          <span className="capitalize">{l.size.toLocaleLowerCase()}</span>
+        ) : (
+          l.size.map((s) => {
+            return (
+              <span className="capitalize" key={s}>
+                {s.toLocaleLowerCase()}{" "}
+              </span>
+            );
+          })
+        )}
+      </div>
+      <div>
+        <span className="font-semibold">Speed - </span>
+        {l.speed}
+      </div>
+      <div>
+        <span className="font-semibold">Stat - </span>
+        {l.stat}
+      </div>
+      <Traits title="Traits" traits={l.traits} />
+      {l.options && <Traits title="Options" traits={l.options} />}
+    </div>
+  );
+};
 
 export default Lineage;
--- fortunes-favor/app/components/Traits.tsx
+++ fortunes-favor/app/components/Traits.tsx
@@ -1,18 +1,23 @@
-import Trait, {trait} from "./blocks/Trait";
+import { CharacterTrait } from "../utils/CharacterTrait";
+import Trait from "./blocks/Trait";
 
-const Traits = ({title, traits}: {title: string, traits: [trait]}) => {
-    return(
-        <div>
-            <span className="text-lg font-semibold">{title}</span>
-            <div className="px-3 border-amber-800 border-l-2 space-y-1">
-            {traits.map(t => {
-                return(
-                    <Trait t={t}/>
-                )
-            })}
-            </div>
-        </div>
-    )
-}
+const Traits = ({
+  title,
+  traits,
+}: {
+  title: string;
+  traits: [CharacterTrait];
+}) => {
+  return (
+    <div>
+      <span className="text-lg font-semibold">{title}</span>
+      <div className="px-3 border-amber-800 border-l-2 space-y-1">
+        {traits.map((t) => {
+          return <Trait t={t} key={t.title} />;
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Traits;
