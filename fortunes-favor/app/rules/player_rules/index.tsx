75e0f992bd633fb3fbf9f4dc093901971bc40f35
LankeyBoard <56012902+LankeyBoard@users.noreply.github.com>
2024-03-05 16:48:43 -0500

    Wrapped the base layout with suspense

--- fortunes-favor/app/rules/player_rules/index.tsx
+++
@@ -1,8 +0,0 @@
-import Layout from "@/app/components/layout";
-import type { ReactElement } from "react";
-
-function Page(page: ReactElement) {
-  return <Layout>{page}</Layout>;
-}
-
-export default Page;
