f14609bf67632f369972051e565cd7aa2486467b
LankeyBoard <56012902+LankeyBoard@users.noreply.github.com>
2024-03-06 11:57:59 -0500

    Search bar always visible now

--- fortunes-favor/app/components/TopNav.tsx
+++ fortunes-favor/app/components/TopNav.tsx
@@ -6,31 +6,7 @@ const TopNav = () => {
       <nav className="bg-white border-gray-200 dark:bg-slate-900 border-b-2 border-b-teal-400">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <div className="flex md:order-2 grow align-right">
-            <button
-              type="button"
-              data-collapse-toggle="navbar-search"
-              aria-controls="navbar-search"
-              aria-expanded="false"
-              className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-amber-200 dark:focus:ring-amber-700 rounded-lg text-sm p-2.5 me-1"
-            >
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
-              <span className="sr-only">Search</span>
-            </button>
-            <div className="flex-auto min-w-96">
+            <div className="flex-auto md:min-w-96 min-w-20">
               <SearchBar />
             </div>
 
