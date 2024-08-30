13bb723c8dc425f063dc0bd4f2856118ae9c27c6
LankeyBoard <56012902+LankeyBoard@users.noreply.github.com>
2024-08-29 22:19:58 -0400

    Improved rules menu nav and changed icons to match style on tailwind

--- fortunes-favor/app/components/blocks/NavHeader.tsx
+++ fortunes-favor/app/components/blocks/NavHeader.tsx
@@ -29,20 +29,14 @@ const NavHeader = () => {
         }}
       >
         <span className="sr-only">Open main menu</span>
-        <svg
-          className="w-5 h-5"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 17 14"
-        >
+        <svg width="24" height="24" fill="none" aria-hidden="true">
           <path
+            d="M12 6v.01M12 12v.01M12 18v.01M12 7a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"
             stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M1 1h15M1 7h15M1 13h15"
-          />
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
         </svg>
       </button>
       {menuVisible && (
--- fortunes-favor/app/components/blocks/RulesNav.tsx
+++ fortunes-favor/app/components/blocks/RulesNav.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import { nav } from "@/app/rules/layout";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { MouseEventHandler, useEffect, useState } from "react";
 
 import useWindowDimensions from "@/app/utils/useWindowDimensions";
 import { isSmallWindow } from "@/app/utils/isSmallWindow";
@@ -11,9 +11,10 @@ import Tooltip from "./Tooltip";
 type navProps = {
   navEl: nav;
   isSub?: boolean;
+  closeMenuIfOpen: () => void;
 };
 
-export const NavElem = ({ navEl, isSub }: navProps) => {
+export const NavElem = ({ navEl, isSub, closeMenuIfOpen }: navProps) => {
   const path = usePathname();
   let isCurrent = false;
   if (path === navEl.href) {
@@ -32,6 +33,10 @@ export const NavElem = ({ navEl, isSub }: navProps) => {
           <Link
             href={navEl.href}
             className="font-light text-lg hover:tracking-wide whitespace-nowrap"
+            onClick={() => {
+              console.log("click");
+              closeMenuIfOpen();
+            }}
           >
             <div
               className={
@@ -59,7 +64,14 @@ export const NavElem = ({ navEl, isSub }: navProps) => {
       )}
       <div className="mx-2">
         {navEl.subroutes?.map((r) => {
-          return <NavElem navEl={r} isSub={true} key={r.title} />;
+          return (
+            <NavElem
+              navEl={r}
+              isSub={true}
+              key={r.title}
+              closeMenuIfOpen={closeMenuIfOpen}
+            />
+          );
         })}
       </div>
     </div>
@@ -67,83 +79,81 @@ export const NavElem = ({ navEl, isSub }: navProps) => {
 };
 
 const NavMenu = ({ navMap }: { navMap: nav[] }) => {
+  const { height, width } = useWindowDimensions();
+  const [menuVisible, setMenuVisible] = useState(true);
+  useEffect(() => {
+    if (!isSmallWindow(width)) {
+      setMenuVisible(true);
+    } else {
+      setMenuVisible(false);
+    }
+  }, [width]);
+  const closeMenuIfOpen = () => {
+    console.log("here");
+    if (isSmallWindow(width)) {
+      console.log("close menu");
+      setMenuVisible(false);
+    }
+  };
   return (
-    <div className="flex-left flex-grow overflow-auto h-[calc(100vh-72px)]">
-      <aside
-        id="sidebar-multi-level-sidebar"
-        className="z-40 w-64 bg-slate-200 dark:bg-slate-950 p-4 overflow-auto "
-        aria-label="Sidebar"
+    <div className="flex-left flex-grow overflow-auto md:h-[calc(100vh-72px)] h-auto w-screen backdrop-blur-sm bg-black/30 p-2">
+      <button
+        type="button"
+        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm backdrop-blur-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100/70 focus:outline-none focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700/70 dark:focus:ring-gray-600"
+        aria-controls="navbar-links"
+        aria-expanded="false"
+        onClick={() => {
+          setMenuVisible(!menuVisible);
+        }}
       >
-        <div className="flex-col my-5">
-          {navMap.map((n) => {
-            return <NavElem navEl={n} key={n.title} />;
-          })}
-        </div>
-      </aside>
+        <span className="sr-only">Open main menu</span>
+        <svg width="24" height="24">
+          <path
+            d="M5 6h14M5 12h14M5 18h14"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+          ></path>
+        </svg>
+      </button>
+      {menuVisible && (
+        <aside
+          id="sidebar-multi-level-sidebar"
+          className="z-40 w-64 bg-slate-200 dark:bg-slate-950 p-4 overflow-auto h-full "
+          aria-label="Sidebar"
+        >
+          <div className="flex-col my-5">
+            {navMap.map((n) => {
+              return (
+                <NavElem
+                  navEl={n}
+                  key={n.title}
+                  closeMenuIfOpen={closeMenuIfOpen}
+                />
+              );
+            })}
+          </div>
+        </aside>
+      )}
     </div>
   );
 };
 
 const RulesNav = ({ navMap }: { navMap: nav[] }) => {
   const { height, width } = useWindowDimensions();
-  const [menuVisible, setMenuVisible] = useState(true);
   const [menuStyle, setMenuStyle] = useState("flex");
   useEffect(() => {
     if (!isSmallWindow(width)) {
-      setMenuStyle("flex");
-      setMenuVisible(true);
+      setMenuStyle("flex h-screen");
     } else {
       setMenuStyle("fixed flex items-start h-screen");
-      setMenuVisible(false);
     }
   }, [width]);
 
-  const handleMenuHideButton = () => {
-    setMenuVisible((prevVisible) => !prevVisible);
-  };
-
   return (
     <div className={menuStyle}>
-      <div className={menuVisible ? "z-50 overflow-hidden" : "hidden"}>
-        <NavMenu navMap={navMap} />
-      </div>
-      <div className="mt-9">
-        <Tooltip message={menuVisible ? "Hide Menu" : "Show Menu"}>
-          <div className={"flex-left z-50 h-fit"}>
-            <div
-              className={
-                menuVisible
-                  ? "-rotate-90 w-auto h-auto m-2"
-                  : "rotate-90 w-auto h-auto m-2"
-              }
-            >
-              <button
-                aria-controls="sidebar-multi-level-sidebar"
-                type="button"
-                onClick={handleMenuHideButton}
-                className="items-center p-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              >
-                <span className="sr-only">Open sidebar</span>
-                <svg
-                  className="w-3 h-3 shrink-0"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 10 6"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M9 5 5 1 1 5"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
-        </Tooltip>
-      </div>
+      <NavMenu navMap={navMap} />
     </div>
   );
 };
