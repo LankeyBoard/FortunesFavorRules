6a8dc16457768efa95c7a07309ea98791b767e9c
LankeyBoard <56012902+LankeyBoard@users.noreply.github.com>
2024-09-11 22:45:46 -0400

    updates based on mikes feedback

--- fortunes-favor/app/rules/page.tsx
+++ fortunes-favor/app/rules/page.tsx
@@ -9,7 +9,7 @@ const rulesPageText: GenericRule[] = [
     ruleType: rule_type.Rule,
     text: [
       {
-        text: "Fortune's Favor is a d20 based system with success, mixed results and failures. At it's simplest a player wants to do something risky, the Game Master (GM) decides how difficult the task is and tells the player. They both work together to figure out what bonuses apply to the roll. The player rolls and based on the result the GM continues to narrate the scene.",
+        text: "Fortune's Favor is a d20 based system with success, mixed results and failures. At its simplest, a player wants to do something risky, the Game Master (GM) decides how difficult the task is and tells the player. They both work together to figure out what bonuses apply to the roll. The player rolls and based on the result the GM continues to narrate the scene.",
         type: "RULE",
       },
     ],
@@ -22,7 +22,7 @@ const rulesPageText: GenericRule[] = [
     ruleType: rule_type.Rule,
     text: [
       {
-        text: "It isn't necessary nor intended for you to read the rules cover to cover. Here are a few different suggested readings based on how much you want to read.",
+        text: "You aren't expected or required to read the rules cover to cover before you play. Here are a few different suggested reading lists.",
         type: "RULE",
       },
     ],
@@ -33,7 +33,33 @@ const rulesPageText: GenericRule[] = [
         ruleType: rule_type.Rule,
         text: [
           {
-            text: "Read [Building a Character](/rules/player_rules#BUILDING-A-PC), [Parts of a Character](/rules/player_rules#WHAT-MAKES-UP-A-CHARACTER) & [Character Resources](/rules/player_rules#CHAR-RESOURCES). Then skim the Lineages, Cultures and Classes and read the ones that interest you.",
+            text: "Read [Building a Character](/rules/player_rules#BUILDING-A-PC), [Parts of a Character](/rules/player_rules#WHAT-MAKES-UP-A-CHARACTER) & [Character Resources](/rules/player_rules#CHAR-RESOURCES). Then skim the [Cultures](/rules/cultures), [Lineages](/rules/lineages) and [Classes](/rules/classes) and read the ones that interest you.",
+            type: "RULE",
+          },
+        ],
+        subRules: [],
+        list: [],
+      },
+      {
+        title: "A Solid Understanding",
+        slug: "SOLID-UNDERSTANDING",
+        ruleType: rule_type.Rule,
+        text: [
+          {
+            text: "Read from [Building a Character](/rules/player_rules#BUILDING-A-PC) until the end of [Combat](/rules/player_rules#COMBAT). Then skim the [Cultures](/rules/cultures), [Lineages](/rules/lineages) and [Classes](/rules/classes) and read the ones that interest you. Then read the [Dying & Last Stand](/rules/player_rules#EDL) & [Healing](/rules/player_rules#NON-MAGICAL-HEALING) sections.",
+            type: "RULE",
+          },
+        ],
+        subRules: [],
+        list: [],
+      },
+      {
+        title: "Running a Game",
+        slug: "RUNNING",
+        ruleType: rule_type.Rule,
+        text: [
+          {
+            text: "You have to read all the rules.",
             type: "RULE",
           },
         ],
